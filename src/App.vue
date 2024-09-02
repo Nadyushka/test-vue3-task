1b8d2d12294cfa53acd1103e670102c78bb07a6b
Nadyushka <12nadya11@gmail.com>
2024-09-02 09:27:36 +0300

    add sorting by rating

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <template>
   <main>
-    <CardList v-for="(item, index) in CardListOptions" :key="index" :options="item" />
+    <div v-for="(item, index) in CardListOptions" :key="index" class="cards">
+        <v-select
+            class="card__sorting"
+            label="Сортировка"
+            variant="solo"
+            bg-color="#D3D3D3FF"
+            clearable
+            item-value="sortType"
+            :items="SortingOptions"
+            @update:modelValue="(sortType) => setSortingForList(index, sortType)"
+        >
+          <template #selection="{ item }">
+            {{ item.title  }}
+            <v-icon
+                :icon="item.raw.direction == 'asc' ? 'mdi-arrow-up' : 'mdi-arrow-down'"
+                class="ml-1"/>
+          </template>
+
+          <template #item="{ item, props }">
+            <v-list-item v-bind="props">
+              <template v-slot:prepend>
+                <v-icon
+                    :icon="item.raw.direction == 'asc' ? 'mdi-arrow-up' : 'mdi-arrow-down'"
+                    class="ml-1" />
+              </template>
+            </v-list-item>
+          </template>
+        </v-select>
+
+      <CardList
+          :options="item"
+      />
+    </div>
   </main>
 </template>
 
@@ -31,6 +63,54 @@
     },
   ]);
 
+  const sortingTypes = {
+    ratingAcs: 1,
+    ratingDesc: 2
+  }
+
+  const SortingOptions = ref([
+    {
+      title: 'По рейтингу',
+      sortType: sortingTypes.ratingAcs,
+    },
+    {
+      title: 'По рейтингу',
+      sortType: sortingTypes.ratingDesc,
+    },
+  ]);
+
+  const setSortingForList = (listIndex, sortType) => {
+    switch (listIndex) {
+      case 0: {
+        firstList.value = firstList.value.sort(sortCallback(sortType));
+        return;
+      }
+      case 1: {
+        secondList.value = secondList.value.sort(sortCallback(sortType));
+        return;
+      }
+      case 2: {
+        lastList.value = lastList.value.sort(sortCallback(sortType));
+        return;
+      }
+      default:
+        console.warn("Invalid list index");
+    }
+  };
+
+  const sortCallback = (sortType) => (a, b) => {
+    switch (sortType) {
+      case sortingTypes.ratingAcs:
+        return a.rating.rate - b.rating.rate;
+
+      case sortingTypes.ratingDesc:
+        return b.rating.rate - a.rating.rate;
+
+      default:
+        return a.id - b.id;
+    }
+  };
+
   const getAllCards = async () => {
     try {
       await axios
@@ -58,4 +138,9 @@
     gap: 5%;
     width: 100%;
   }
+
+  .card__title {
+    margin-bottom: 16px;
+  }
+
 </style>
